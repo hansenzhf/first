2ab9e76f814a9c971d809a6f6127ddaa584dc222
hansenzhf <54924508+hansenzhf@users.noreply.github.com>
2020-12-05 11:14:11 +0800

    修改：适配windows环境
    修改：适配windows环境

--- AK_AW/readme.txt
+++
@@ -1,1 +0,0 @@
-Here we store the Interface used by AK_Test.
---
+++ lib/get_stock_data.py
@@ -0,0 +1,23 @@
+# -*-coding:utf-8-*-
+__author__ = 'hank'
+
+import pandas as pd
+import numpy as np
+import akshare as ak
+
+class GetStockData(object):
+    def get_price_data(self, code):
+        pingan = ak.stock_zh_a_daily(symbol="sh601318", adjust="qfq")
+        df3 = pingan.reset_index().iloc[-30:,:6]  #取过去30天数据
+        df3 = df3.dropna(how='any').reset_index(drop=True) #去除空值且从零开始编号索引
+        df3 = df3.sort_values(by='date', ascending=True)
+        print(df3.info())
+
+        # 均线数据
+        df3['5'] = df3.close.rolling(5).mean()
+        df3['10'] = df3.close.rolling(10).mean()
+
+        df3.tail()
+
+if __name__ == "__main__":
+    GetStockData().get_price_data("sh601318")
